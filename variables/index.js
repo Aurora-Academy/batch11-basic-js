595e23906b6d18ee81ded30b46a7f0200add42d6
Raktim Shrestha <rakimsth@gmail.com>
2024-10-23 18:57:51 +0545

    learned about variables

---
+++ variables/index.js
@@ -0,0 +1,24 @@
+// REPL (Read, Evaluate, Print, Loop)
+
+// Print
+console.log("Hello world");
+
+// Variables
+// named storage for data
+
+// ES5
+var person = "raktim";
+var bat = "red bat";
+person = "shyam";
+console.log(person, bat);
+
+// ES6
+// let, const
+
+const apple = "red apple";
+// apple = "blue apple";
+console.log(apple);
+
+let ball = "red ball";
+ball = "blue ball";
+console.log(ball);
